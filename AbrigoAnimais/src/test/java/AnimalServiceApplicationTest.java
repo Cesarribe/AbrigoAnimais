c5c2a90185c9465be96539133b8c1260021e9c1a
Cesarribe <Cesar.ribeiro_@hotmail.com>
2024-11-16 17:01:25 -0300

    commit

---
+++ AbrigoAnimais/src/test/java/AnimalServiceApplicationTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+public class AnimalServiceApplicationTest {
+    @SpringBootTest
+    class AnimalServiceApplicationTests {
+
+        @Test
+        void contextLoads() {
+        }
+
+    }
+}
